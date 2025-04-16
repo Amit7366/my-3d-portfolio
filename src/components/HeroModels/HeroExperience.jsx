f8023aa85a630361f335d7488cd6eabbf397f00f
Amit7366 <omit9090@gmail.com>
2025-04-17 00:42:11 +0600

    hero section done

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -2,26 +2,37 @@ import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React from "react";
 import { useMediaQuery } from "react-responsive";
+import { Room } from "./Room";
+import HeroLights from "./HeroLights";
+import Particles from "./Particles";
 
 function HeroExperience() {
-    const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
-    const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
-      <ambientLight intensity={0.2} color="#1a1a40" />
-      <directionalLight position={[5, 5, 5]} intensity={5} />
-        <OrbitControls
-            enablePan={false}
-            enableZoom={!isTablet}
-            maxDistance={20}
-            minDistance={5}
-            minPolarAngle={Math.PI / 5}
-            maxPolarAngle={Math.PI / 2}
-        />
-      <mesh>
+      
+      <OrbitControls
+        enablePan={false}
+        enableZoom={!isTablet}
+        maxDistance={20}
+        minDistance={5}
+        minPolarAngle={Math.PI / 5}
+        maxPolarAngle={Math.PI / 2}
+      />
+      <HeroLights/>
+      <Particles count={isMobile ? 100 : 200}/>
+      <group
+        scale={isMobile ? 0.7 : 1}
+        position={[0,-3.5,0]}
+        rotation={[0, -Math.PI / 4, 0]}
+      >
+        <Room />
+      </group>
+      {/* <mesh>
         <boxGeometry args={[2, 2, 2]} />
         <meshStandardMaterial color={"#ffffff"}/>
-      </mesh>
+      </mesh> */}
     </Canvas>
   );
 }
